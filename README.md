b6d1b19d96378e425f20e1a100a9cf02522dd2e0
Erik Slovak <erik.slovak@technologystudio.sk>
2025-01-24 15:50:14 +0100

    feat: initial implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+![npm](https://img.shields.io/npm/v/eslint-config-txo-package-graphql)
+![codecov](https://img.shields.io/codecov/c/github/technology-studio/eslint-config-txo-package-graphql)
+# Eslint config txo package graphql
+
+Eslint config txo package graphql
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2024-11-11T16:26:28+01:00
+ * @Copyright: Technology Studio
+**/
+
+import {
+  jestConfig,
+  stylisticConfig,
+  typescriptConfigList,
+  type TSESLint,
+  typescriptEslintConfig,
+} from 'eslint-config-txo-typescript'
+
+const defaultConfigList = typescriptEslintConfig(
+  {
+    files: ['**/*.js', '**/*.ts'],
+    extends: [
+      // eslint-disable-next-line @typescript-eslint/no-deprecated -- remove when migrated to prettier
+      stylisticConfig,
+      ...typescriptConfigList,
+    ],
+  },
+  {
+    files: ['seed/**/*.ts', 'seed/**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './seed/tsconfig.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './seed/tsconfig.json',
+        },
+      },
+    },
+  },
+  {
+    files: ['app/**/*.ts', 'app/**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './app/tsconfig.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './app/tsconfig.json',
+        },
+      },
+    },
+  },
+)
+
+const jestConfigList = typescriptEslintConfig(
+  {
+    files: ['__tests__/**/*.ts'],
+    extends: [
+      jestConfig,
+    ],
+  },
+)
+
+export const configList: TSESLint.FlatConfig.Config[] = [
+  ...defaultConfigList,
+  ...jestConfigList,
+  {
+    ignores: [
+      '.releaserc.js',
+      'cli.js',
+      'commitlint.config.js',
+      'coverage/**/*',
+      'eslint-ci.config.js',
+      'eslint.config.js',
+      'jest.config.js',
+      'lib/**/*',
+      'node_modules',
+      'release.config.js',
+      '__tests__/GraphqlCodeGen/index.ts',
+    ],
+  },
+]
